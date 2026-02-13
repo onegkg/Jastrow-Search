1c47c55031da88b43f3b8ab995fd7c4033aff1ec
onegkg <onegkg@gmail.com>
2026-02-13 16:52:19 -0500

    Setup for gh-pages deployment with vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import { serve } from "bun";
-import index from "./index.html";
+import index from "../index.html";
 
 const server = serve({
-	routes: {
-		// Serve index.html for all unmatched routes.
-		"/*": index,
-	},
+  routes: {
+    // Serve index.html for all unmatched routes.
+    "/*": index,
+  },
 
-	development: process.env.NODE_ENV !== "production" && {
-		// Enable browser hot reloading in development
-		hmr: true,
+  development: process.env.NODE_ENV !== "production" && {
+    // Enable browser hot reloading in development
+    hmr: true,
 
-		// Echo console logs from the browser to the server
-		console: true,
-	},
+    // Echo console logs from the browser to the server
+    console: true,
+  },
 });
 
 console.log(`🚀 Server running at ${server.url}`);
